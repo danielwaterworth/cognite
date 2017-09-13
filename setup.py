c9502a1f7e80d0431a510e3e583588794ad6e15b
Daniel Waterworth <da.waterworth@gmail.com>
2017-09-13 15:04:03 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "cognite",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Daniel Waterworth",
     author_email = "daniel@manganizeme.com",
     description = "",
