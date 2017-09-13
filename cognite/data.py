561802a443762c6181d92a85e0c0820897d93d25
Daniel Waterworth <da.waterworth@gmail.com>
2017-09-13 11:21:22 -0500

    loading and dumping

--- cognite/data.py
+++ cognite/data.py
@@ -1,4 +1,7 @@
+import functools
 import mxnet as mx
+import operator
+import struct
 
 def elementwise(a, b, fn):
     assert (isinstance(a, dict) and isinstance(b, dict)) or (isinstance(a, mx.ndarray.NDArray) and isinstance(b, mx.ndarray.NDArray))
@@ -43,3 +46,52 @@ def check(data):
             all(map(check, data.values()))
     else:
         return False
+
+def dump(data, fd):
+    if isinstance(data, dict):
+        fd.write(struct.pack('!B', 1))
+        fd.write(struct.pack('!Q', len(data)))
+        for key, value in data.items():
+            assert isinstance(key, str)
+            key_bytes = key.encode('utf8')
+            fd.write(struct.pack('!Q', len(key_bytes)))
+            fd.write(key_bytes)
+            dump(value, fd)
+    elif isinstance(data, mx.ndarray.NDArray):
+        fd.write(struct.pack('!B', 2))
+        fd.write(struct.pack('!Q', len(data.shape)))
+        for x in data.shape:
+            fd.write(struct.pack('!Q', x))
+        size = functools.reduce(operator.mul, data.shape, 1)
+        values = data.reshape((size,))
+        for value in values.asnumpy():
+            fd.write(struct.pack('!f', value))
+    else:
+        raise TypeError('not dumpable')
+
+def load(fd):
+    t = struct.unpack('!B', fd.read(1))[0]
+    if t == 1:
+        n = struct.unpack('!Q', fd.read(8))[0]
+        output = {}
+        for i in range(n):
+            n = struct.unpack('!Q', fd.read(8))[0]
+            key = fd.read(n).decode('utf8')
+            value = load(fd)
+            output[key] = value
+        return output
+    elif t == 2:
+        n = struct.unpack('!Q', fd.read(8))[0]
+        size = 1
+        shape = []
+        for i in range(n):
+            n = struct.unpack('!Q', fd.read(8))[0]
+            shape.append(n)
+            size *= n
+        values = []
+        for i in range(size):
+            f = struct.unpack('!f', fd.read(4))[0]
+            values.append(f)
+        return mx.ndarray.array(values).reshape(shape)
+    else:
+        raise TypeError('unknown data type')
